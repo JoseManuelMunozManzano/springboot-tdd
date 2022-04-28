f5070958d7d3a665babaca98f71f9766c6f96da2
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-04-28 06:51:36 +0200

    Se crea la clase Service y Repository (GREEN)

--- src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -1,23 +1,35 @@
 package com.luv2code.springmvc;
 
 import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.repository.StudentDao;
+import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 // Con la anotación @TestPropertySource se cargan, durante los test, ficheros de properties
 @TestPropertySource("/application.properties")
 @SpringBootTest
 public class StudentAndGradeServiceTest {
 
+    @Autowired
+    private StudentAndGradeService studentService;
+
+    @Autowired
+    private StudentDao studentDao;
+
     @Test
     void createStudentService() {
-        // No existe StudentService, asi que esto falla (RED)
+        // Se crea StudentAndGradeService y lo necesario para que el test pase (GREEN)
         studentService.createStudent("José Manuel", "Muñoz", "jmunoz@gmail.es");
 
-        // CollegeStudent student = studentDao.findByEmailAddress("jmunoz@gmail.es");
+        // Se crea el método findByEmailAddress
+        CollegeStudent student = studentDao.findByEmailAddress("jmunoz@gmail.es");
 
-        // assertEquals("jmunoz@gmail.es", student.getEmailAddress(), "find by email");
+        assertEquals("jmunoz@gmail.es", student.getEmailAddress(), "find by email");
     }
 }
 
