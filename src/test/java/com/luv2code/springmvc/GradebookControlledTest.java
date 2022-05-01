1f4bdbfdf5dc6b920cc047f5261229ea02dcd195
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-05-01 11:30:29 +0200

    Student Inf and Grades. student info. Test falla (RED)

--- src/test/java/com/luv2code/springmvc/GradebookControlledTest.java
+++ src/test/java/com/luv2code/springmvc/GradebookControlledTest.java
@@ -173,6 +173,30 @@ public class GradebookControlledTest {
         ModelAndViewAssert.assertViewName(mav, "error");
     }
 
+    @Test
+    void studentInformationHttpRequest() throws Exception {
+        assertTrue(studentDao.findById(1).isPresent());
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/studentInformation/{id}", 1))
+                .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "studentInformation");
+    }
+
+    @Test
+    void studentInformationHttpStudentDoesNotExistRequest() throws Exception {
+        assertFalse(studentDao.findById(0).isPresent());
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/studentInformation/{id}", 0))
+                .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav =  mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "error");
+    }
+
     @AfterEach
     void setupAfterTransaction() {
         jdbc.execute(sqlDeleteStudent);
