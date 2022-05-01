7f8cc5fb11daf1bd2820b03617633cc95315cda9
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-05-01 16:02:39 +0200

    Student Inf and Grades. Borrar grade existente. Test falla (RED)

--- src/test/java/com/luv2code/springmvc/GradebookControlledTest.java
+++ src/test/java/com/luv2code/springmvc/GradebookControlledTest.java
@@ -2,6 +2,8 @@ package com.luv2code.springmvc;
 
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.models.GradebookCollegeStudent;
+import com.luv2code.springmvc.models.MathGrade;
+import com.luv2code.springmvc.repository.MathGradesDao;
 import com.luv2code.springmvc.repository.StudentDao;
 import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.junit.jupiter.api.AfterEach;
@@ -26,6 +28,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -54,6 +57,9 @@ public class GradebookControlledTest {
 
     @Autowired
     private StudentAndGradeService studentService;
+
+    @Autowired
+    private MathGradesDao mathGradesDao;
 
     @Value("${sql.script.create.student}")
     private String sqlAddStudent;
@@ -253,6 +259,29 @@ public class GradebookControlledTest {
         ModelAndView mav = mvcResult.getModelAndView();
 
         ModelAndViewAssert.assertViewName(mav, "error");
+    }
+
+    @Test
+    void deleteAValidGradeHttpRequest() throws Exception {
+        // Nos aseguramos que el grade existe
+        Optional<MathGrade> mathGrade = mathGradesDao.findById(1);
+
+        assertTrue(mathGrade.isPresent());
+
+        // Hacemos el request del delete
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .get("/grades/{id}/{gradeType}", 1, "math"))
+                .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "studentInformation");
+
+        // Confirmar que el grade fue realmente borrado
+        mathGrade = mathGradesDao.findById(1);
+
+        assertFalse(mathGrade.isPresent());
+
     }
 
     @AfterEach
